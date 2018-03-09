45d49118f5ebec3c9d8f71f315254428aaf32584
Aaron <acp.aaron512@gmail.com>
2018-03-09 19:20:04 +0530

    initial push

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\all_code_and_extra\codefeb18onwards\django\twitterlike\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
